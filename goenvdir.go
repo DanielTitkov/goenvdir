882b78e5702b666ebecb687833e9a948ebc9b67c
DanielTitkov <mytasmic@gmail.com>
2019-08-01 13:47:48 +0300

    working version

--- goenvdir.go
+++ goenvdir.go
@@ -1,9 +1,63 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"os/exec"
+	"path"
 )
 
+func collectEnvVars(envPath string) ([]string, error) {
+	var envVars []string
+	files, err := ioutil.ReadDir(envPath)
+	if err != nil {
+		return envVars, err
+	}
+
+	for _, file := range files {
+		name := file.Name()
+		path := path.Join(envPath, name)
+		data, err := ioutil.ReadFile(path)
+		if err != nil {
+			return envVars, err
+		}
+		envVars = append(envVars, name+"="+string(data))
+	}
+	return envVars, nil
+}
+
+func setupCmdEnv(program string, envVars []string, overrideEnv bool) *exec.Cmd {
+	cmd := exec.Command(program)
+	if overrideEnv {
+		cmd.Env = envVars
+	} else {
+		cmd.Env = append(envVars, os.Environ()...)
+	}
+	return cmd
+}
+
 func main() {
-	fmt.Println("Foo")
+	overrideEnv := flag.Bool("i", false, "Override env")
+	flag.Parse()
+
+	if flag.NArg() < 2 {
+		log.Fatal("Provide envdir and program to execute")
+		os.Exit(1)
+	}
+	envPath, program := flag.Args()[0], flag.Args()[1]
+
+	envVars, err := collectEnvVars(envPath)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	cmd := setupCmdEnv(program, envVars, *overrideEnv)
+	out, err := cmd.Output()
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(string(out))
 }
